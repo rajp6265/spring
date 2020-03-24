e845d078c23d0dd689804df7858eb251ee408ffc
Raj Patel <patelraj.6265@yahoo.in>
2020-03-24 11:29:52 +0530

    up2date

--- target/classes/data.sql
+++
@@ -1,7 +0,0 @@
-insert into alien values(101,'rajiv','java');
-insert into alien values(102,'ramukaka','.net');
-insert into alien values(103,'ranjan','android');
-insert into alien values(104,'rajat','java');
-insert into alien values(105,'ram','.net');
-insert into alien values(106,'aju','android');
-insert into alien values(107,'ramesh','java');
